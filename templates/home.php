e063db76dbcddd6a087aab9baba4a9498302099d
Marco <marcomartinezavila@gmail.com>
2016-11-27 01:40:30 +0100

    Added the structure of the products

--- templates/home.php
+++ templates/home.php
@@ -20,6 +20,32 @@
 <div class="products">
     <div class="content grid">
         <div class="product">
+        	<div class="image product-element">
+        		<img src="./resources/doriginal/oxford-college-1.jpg" alt="Oxford College 1">
+        	</div>
+        	<div class="price product-element">aaa</div>
+        	<div class="buttons product-element">bbb</div>
+        </div>
+        <div class="product">
+        	<div class="image product-element">
+        		<img src="./resources/hawkers/warwick-blue-1.jpg" alt="Warwick Blue 1">
+        	</div>
+        	<div class="price product-element">aaa</div>
+        	<div class="buttons product-element">bbb</div>
+        </div>
+        <div class="product">
+        	<div class="image product-element">
+        		<img src="./resources/doriginal/andrassy-avenue-1.jpg" alt="Andrassy Avenue 1">
+        	</div>
+        	<div class="price product-element">aaa</div>
+        	<div class="buttons product-element">bbb</div>
+        </div>
+        <div class="product">
+        	<div class="image product-element">
+        		<img src="./resources/hawkers/carbon-black-nebula-1.jpg" alt="Carbon Black Nebula 1">
+        	</div>
+        	<div class="price product-element">aaa</div>
+        	<div class="buttons product-element">bbb</div>
         </div>
     </div><!-- .content -->
 </div><!-- .products -->
