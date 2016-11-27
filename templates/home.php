05004512b57288d48e98e33ccfc10015bec21a72
Marco <marcomartinezavila@gmail.com>
2016-11-27 17:45:06 +0100

    Everything related to the products (and the promo) has been done.

--- templates/home.php
+++ templates/home.php
@@ -22,30 +22,179 @@
         <div class="product">
         	<div class="image product-element">
         		<img src="./resources/doriginal/oxford-college-1.jpg" alt="Oxford College 1">
+        		<img class="secondary" src="./resources/doriginal/oxford-college-2.jpg" alt="Oxford College 2">
         	</div>
-        	<div class="price product-element">aaa</div>
-        	<div class="buttons product-element">bbb</div>
+        	<div class="price product-element align-right">
+        		<div>
+        			<span class="main-font original-price">42€</span>
+        			<span class="main-font actual-price">25€</span>
+        		</div>
+        		<span class="product-font">Oxford College</span>
+        	</div>
+        	<div class="buttons product-element main-font">
+        		<a href="./">AÑADIR AL CARRO</a>
+        		<a href="./">+ INFO</a>
+        	</div>
         </div>
         <div class="product">
         	<div class="image product-element">
         		<img src="./resources/hawkers/warwick-blue-1.jpg" alt="Warwick Blue 1">
+        		<img class="secondary" src="./resources/hawkers/warwick-blue-2.jpg" alt="Warwick Blue 2">
         	</div>
-        	<div class="price product-element">aaa</div>
-        	<div class="buttons product-element">bbb</div>
+        	<div class="price product-element align-right">
+        		<div>
+        			<span class="main-font original-price">40€</span>
+        			<span class="main-font actual-price">20€</span>
+        		</div>
+        		<span class="product-font">Clear Blue Warwick X</span>
+        	</div>
+        	<div class="buttons product-element main-font">
+        		<a href="./">AÑADIR AL CARRO</a>
+        		<a href="./">+ INFO</a>
+        	</div>
         </div>
         <div class="product">
         	<div class="image product-element">
         		<img src="./resources/doriginal/andrassy-avenue-1.jpg" alt="Andrassy Avenue 1">
+        		<img class="secondary" src="./resources/doriginal/andrassy-avenue-2.jpg" alt="Andrassy Avenue 2">
         	</div>
-        	<div class="price product-element">aaa</div>
-        	<div class="buttons product-element">bbb</div>
+        	<div class="price product-element align-right">
+        		<div>
+        			<span class="main-font original-price">42€</span>
+        			<span class="main-font actual-price">25€</span>
+        		</div>
+        		<span class="product-font">Oxford College</span>
+        	</div>
+        	<div class="buttons product-element main-font">
+        		<a href="./">AÑADIR AL CARRO</a>
+        		<a href="./">+ INFO</a>
+        	</div>
         </div>
         <div class="product">
         	<div class="image product-element">
         		<img src="./resources/hawkers/carbon-black-nebula-1.jpg" alt="Carbon Black Nebula 1">
+        		<img class="secondary" src="./resources/hawkers/carbon-black-nebula-1.jpg" alt="Carbon Black Nebula 1">
         	</div>
-        	<div class="price product-element">aaa</div>
-        	<div class="buttons product-element">bbb</div>
+        	<div class="price product-element align-right">
+        		<div>
+        			<span class="main-font original-price">40€</span>
+        			<span class="main-font actual-price">20€</span>
+        		</div>
+        		<span class="product-font">Carbon Black Nebula</span>
+        	</div>
+        	<div class="buttons product-element main-font">
+        		<a href="./">AÑADIR AL CARRO</a>
+        		<a href="./">+ INFO</a>
+        	</div>
+        </div>
+        <div class="publi">
+        	<div class="content">
+        		<span class="logo-font">Envíos gratis a partir de 50€</span>
+        		<span class="main-font">*solo para zapatillas</span>
+        		<img src="./img/logo/logo-white.png">
+        	</div>
+        </div>
+        <div class="product">
+        	<div class="image product-element">
+        		<img src="./resources/doriginal/yale-college-1.jpg" alt="Yale College 1">
+        		<img class="secondary" src="./resources/doriginal/yale-college-2.jpg" alt="Yale College 2">
+        	</div>
+        	<div class="price product-element align-right">
+        		<div>
+        			<span class="main-font original-price">42€</span>
+        			<span class="main-font actual-price">25€</span>
+        		</div>
+        		<span class="product-font">Oxford College</span>
+        	</div>
+        	<div class="buttons product-element main-font">
+        		<a href="./">AÑADIR AL CARRO</a>
+        		<a href="./">+ INFO</a>
+        	</div>
+        </div>
+        <div class="product">
+        	<div class="image product-element">
+        		<img src="./resources/hawkers/diamond-black-yellow-1.jpg" alt="Diamond Black Yellow 1">
+        		<img class="secondary" src="./resources/hawkers/diamond-black-yellow-2.jpg" alt="Diamond Black Yellow 2">
+        	</div>
+        	<div class="price product-element align-right">
+        		<div>
+        			<span class="main-font original-price">40€</span>
+        			<span class="main-font actual-price">20€</span>
+        		</div>
+        		<span class="product-font">Clear Blue Warwick X</span>
+        	</div>
+        	<div class="buttons product-element main-font">
+        		<a href="./">AÑADIR AL CARRO</a>
+        		<a href="./">+ INFO</a>
+        	</div>
+        </div>
+        <div class="product">
+        	<div class="image product-element">
+        		<img src="./resources/doriginal/waimea-colors-1.jpg" alt="Waimea Colors 1">
+        		<img class="secondary" src="./resources/doriginal/waimea-colors-2.jpg" alt="Waimea Colors 2">
+        	</div>
+        	<div class="price product-element align-right">
+        		<div>
+        			<span class="main-font original-price">42€</span>
+        			<span class="main-font actual-price">25€</span>
+        		</div>
+        		<span class="product-font">Oxford College</span>
+        	</div>
+        	<div class="buttons product-element main-font">
+        		<a href="./">AÑADIR AL CARRO</a>
+        		<a href="./">+ INFO</a>
+        	</div>
+        </div>
+        <div class="product">
+        	<div class="image product-element">
+        		<img src="./resources/hawkers/carbon-black-blue-1.jpg" alt="Carbon Black Blue 1">
+        		<img class="secondary" src="./resources/hawkers/carbon-black-blue-2.jpg" alt="Carbon Black Blue 2">
+        	</div>
+        	<div class="price product-element align-right">
+        		<div>
+        			<span class="main-font original-price">42€</span>
+        			<span class="main-font actual-price">25€</span>
+        		</div>
+        		<span class="product-font">Oxford College</span>
+        	</div>
+        	<div class="buttons product-element main-font">
+        		<a href="./">AÑADIR AL CARRO</a>
+        		<a href="./">+ INFO</a>
+        	</div>
+        </div>
+        <div class="product">
+        	<div class="image product-element">
+        		<img src="./resources/doriginal/floripa-city-1.jpg" alt="Floripa City 1">
+        		<img class="secondary" src="./resources/doriginal/floripa-city-2.jpg" alt="Floripa City 2">
+        	</div>
+        	<div class="price product-element align-right">
+        		<div>
+        			<span class="main-font original-price">40€</span>
+        			<span class="main-font actual-price">20€</span>
+        		</div>
+        		<span class="product-font">Clear Blue Warwick X</span>
+        	</div>
+        	<div class="buttons product-element main-font">
+        		<a href="./">AÑADIR AL CARRO</a>
+        		<a href="./">+ INFO</a>
+        	</div>
+        </div>
+        <div class="product">
+        	<div class="image product-element">
+        		<img src="./resources/hawkers/lotto-turquoise-1.jpg" alt="Lotto Turquoise 1">
+        		<img class="secondary" src="./resources/hawkers/lotto-turquoise-2.jpg" alt="Lotto Turquoise 2">
+        	</div>
+        	<div class="price product-element align-right">
+        		<div>
+        			<span class="main-font original-price">42€</span>
+        			<span class="main-font actual-price">25€</span>
+        		</div>
+        		<span class="product-font">Oxford College</span>
+        	</div>
+        	<div class="buttons product-element main-font">
+        		<a href="./">AÑADIR AL CARRO</a>
+        		<a href="./">+ INFO</a>
+        	</div>
         </div>
     </div><!-- .content -->
 </div><!-- .products -->
